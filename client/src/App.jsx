2d69f63e3936f6890ae1fe724ddd94bbe7b546bd
quokkprruk <spurinruk@gmail.com>
2025-01-09 13:23:08 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// rfce shortcut
+// Frontend
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+//Backend for getting data
+
+//Example
+// app.get("/api", (req,res)=>{
+//         res.json({});
+// })
