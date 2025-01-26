3cb6aa606bd3aa7b2523f9fb2c33acb4c81df052
Anthony <149127115+gt-asin@users.noreply.github.com>
2025-01-26 08:10:49 -0500

    reorganized and moved hashing and password comparison functions to one location

---
+++ server/middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (enteredPassword, hashedPassword) => {
+  return bcrypt.compare(enteredPassword, hashedPassword);
+};
+
+module.exports = { authenticateToken, hashPassword, comparePassword };
