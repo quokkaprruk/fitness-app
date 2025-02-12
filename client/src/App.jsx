d91dfa20f0c75a68c0eca259ce3009ffb5b67a14
fzinsha <113274668+fzinsha@users.noreply.github.com>
2025-02-12 13:32:35 -0500

    contact pg complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,11 @@ import TrainerHome from "./pages/TrainerHome";
 import AdminHome from "./pages/AdminHome";
 import Login from "./pages/Login";
 import ClassList from "./pages/ClassListing";
-import Signup from "./pages/SignUp"; 
+import Signup from "./pages/SignUp";
 import MembershipPage from "./pages/MembershipPage";
 import PricingPage from "./pages/PricingPage";
 import RegistrationForm from "./pages/RegistrationForm";
+import ContactPage from "./pages/ContactPage";
 
 const App = () => {
   return (
@@ -22,13 +23,16 @@ const App = () => {
         <Route path="/login" element={<Login />} /> {/* Login Page */}
         <Route path="/classes" element={<ClassList />} /> {/* Class Listing */}
         <Route path="/signup" element={<Signup />} /> {/* Signup Page */}
-        <Route path="/membership" element={<MembershipPage />} /> {/* Membership Page */}
+        <Route path="/membership" element={<MembershipPage />} />{" "}
+        {/* Membership Page */}
         <Route path="/pricing" element={<PricingPage />} /> {/* Pricing Page */}
-        <Route path="/register" element={<RegistrationForm />} /> {/* RegistrationForm */}
+        <Route path="/register" element={<RegistrationForm />} />{" "}
+        {/* RegistrationForm */}
+        <Route path="/contact" element={<ContactPage />} />{" "}
+        {/* Contact Page */}
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
