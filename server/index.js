fd601090c4042d93e27bc96930b98378ecd6a338
quokkprruk <spurinruk@gmail.com>
2025-02-11 23:43:35 -0500

    index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const path = require("path"); // for referencing the static files
-// for referencing the static files
+// const path = require("path"); // for referencing the static files
+// // for referencing the static files
+require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
 //import API routes
@@ -11,53 +12,30 @@ const scheduleRoutes = require("./routes/schedule");
 const logger = require("./middleware/logger");
 const pino = require("pino-http")({ logger });
 
-require("dotenv").config();
-
 const app = express();
 
 app.use(pino);
 app.use(cors());
-app.use(express.json());
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  logger.info(`Server running on http://localhost:${PORT}`);
+});
+
 const mongoURL = `${process.env.MONGO_URL}/${process.env.DB_NAME}`;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURL);
-    logger.info("MongoDB connection success!");
-  } catch (err) {
-    logger.error("MongoDB connection failed");
-  }
-};
+mongoose
+  .connect(mongoURL)
+  .then(() => logger.info("MongoDB connection success!"))
+  .catch((error) => logger.error("MongoDB connection failed"));
 
+app.use(express.json());
 // app.get("/", (req, res) => {
 //   res.send("Welcome to the API!");
-// });
-
-// Handle the root route to send the index.html
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "index.html"));
 // });
 
 app.use("/api/users", userRoutes);
 app.use("/api/trainers", trainerRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/schedule", scheduleRoutes);
-
-// Serve static files in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/dist")));
-
-//   // Send index.html for all other requests (client-side routing)
-//   app.get("*", (req, res) => {
-//     return res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-//   });
-// }
-
-// Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  connectDB();
-  logger.info(`Server running on http://localhost:${PORT}`);
-});
-
-module.exports = app;
