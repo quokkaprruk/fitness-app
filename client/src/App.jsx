4d3fbbda6faa8a9e4642c5c5ea0fa5ea85d002d5
Ravneet Kaur <2284ravneetkaur@gmail.com>
2025-03-16 02:46:51 -0400

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,6 @@ import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
 import { AuthProvider } from "./context/AuthContext";
-import CommunityPage from "./pages/CommunityPage";
 
 const App = () => {
   return (
@@ -67,8 +66,7 @@ const App = () => {
             {/* <Route path="/payment" element={<Payment />} /> */}
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/admin/create-trainer" element={<CreateTrainer />} />
-            <Route path="/community" element={<CommunityPage />} />
-      </Routes>
+            
             <Route
               path="/admin/post-announcement"
               element={<PostAnnouncement />}
