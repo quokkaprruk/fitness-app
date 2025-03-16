38bc16f804511ce7f3acc099d9f93438c8770b1c
Anthony <149127115+gt-asin@users.noreply.github.com>
2025-03-16 14:35:34 -0400

    syntax fix

--- server/routes/trainer.js
+++ server/routes/trainer.js
@@ -8,14 +8,14 @@ router.get("/", async (req, res) => {
   try {
     const trainers = await Trainer.find();
     if (trainers.length > 0) {
-      logger.info(Successfully found ${trainers.length} trainer(s).);
+      logger.info(`Successfully found ${trainers.length} trainer(s).`);
       res.status(200).json(trainers);
     } else {
       logger.info("No trainers found");
       res.status(200).json([]);
     }
   } catch (error) {
-    logger.error(Error fetching trainers: ${error.message});
+    logger.error(`Error fetching trainers: ${error.message}`);
     res.status(500).json({ message: error.message });
   }
 });
@@ -41,11 +41,11 @@ router.post("/", async (req, res) => {
     });
 
     await newTrainer.save();
-    logger.info(Trainer ${firstName} ${lastName} created successfully.);
+    logger.info(`Trainer ${firstName} ${lastName} created successfully.`);
     res.status(201).json({ message: "Trainer created successfully", trainer: newTrainer });
   } catch (error) {
      console.error("FULL ERROR >>>", error);
-    logger.error(Error creating trainer: ${error.message});
+    logger.error(`Error creating trainer: ${error.message}`);
     res.status(500).json({ message: "Error creating trainer", error: error.message });
   }
 });
