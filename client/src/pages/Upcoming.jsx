b38a06760f8b15c2a403831768283fd49d3b9d2d
fzinsha <113274668+fzinsha@users.noreply.github.com>
2025-03-16 22:12:47 -0400

    reverted to old versions

--- client/src/pages/Upcoming.jsx
+++ client/src/pages/Upcoming.jsx
@@ -6,29 +6,25 @@ const Upcoming = () => {
   const [reservedClasses, setReservedClasses] = useState([]);
 
   useEffect(() => {
-    // Retrieve reserved classes from localStorage
     const storedReservations =
       JSON.parse(localStorage.getItem("reservedClasses")) || [];
     setReservedClasses(storedReservations);
   }, []);
 
+  // Function to cancel a reservation with confirmation
   const handleCancel = (classId) => {
     const confirmCancel = window.confirm(
       "Are you sure you want to cancel this booking?"
     );
     if (confirmCancel) {
-      // Remove the canceled reservation
       const updatedReservations = reservedClasses.filter(
-        (classItem) => classItem._id !== classId
+        (classItem) => classItem.classId !== classId
       );
       setReservedClasses(updatedReservations);
-
-      // Update localStorage
       localStorage.setItem(
         "reservedClasses",
         JSON.stringify(updatedReservations)
       );
-
       alert("Reservation canceled.");
     }
   };
@@ -48,7 +44,7 @@ const Upcoming = () => {
               (a, b) => new Date(a.startDateTime) - new Date(b.startDateTime)
             ) // Sort in ascending order
             .map((classItem) => (
-              <li key={classItem._id} className="class-item">
+              <li key={classItem.classId} className="class-item">
                 <h3 className="reserved-classes-name">
                   {classItem.classType} - {classItem.difficultyLevel}
                 </h3>
@@ -62,7 +58,7 @@ const Upcoming = () => {
                 </p>
                 <button
                   className="cancel-booking-button"
-                  onClick={() => handleCancel(classItem._id)}
+                  onClick={() => handleCancel(classItem.classId)}
                 >
                   Cancel Reservation
                 </button>
