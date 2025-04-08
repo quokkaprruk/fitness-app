607262fda501eeffef7f25163f01b88e70cc1df0
quokkprruk <spurinruk@gmail.com>
2025-04-08 02:29:52 -0400

    manual cors config

--- server/index.js
+++ server/index.js
@@ -35,23 +35,27 @@ app.use(express.json());
 
 app.use(pino);
 
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://fitness-app-frontend-prj666.vercel.app",
-  ];
+// app.use((req, res, next) => {
+//   const allowedOrigins = [
+//     "http://localhost:5173",
+//     "https://fitness-app-frontend-prj666.vercel.app",
+//   ];
 
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header("Access-Control-Allow-Origin", origin);
+//   }
 
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  next();
-});
+//   if (req.method === "OPTIONS") {
+//     return res.status(200).end();
+//   }
+
+//   next();
+// });
 
 const mongoURL = `${process.env.MONGO_URL}/${process.env.DB_NAME}`;
 
